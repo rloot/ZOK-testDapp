9b521f452b15b8222d22b5165bb99684aec80d4b
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2023-08-03 14:20:55 -0300

    wip

--- src/Test.ts
+++ src/Test.ts
@@ -7,7 +7,7 @@ import {
   Bool,
   Provable,
 } from 'snarkyjs';
-import { TestStruct } from './generated/TestStruct';
+import { TestStruct } from './generated/TestStruct.js';
 
 export class Test extends SmartContract {
   @state(TestStruct) pep = State<TestStruct>();
@@ -16,49 +16,51 @@ export class Test extends SmartContract {
     super.init();
 
     const p = new TestStruct(
-      new Field(0),
-      new Field(0),
-      new Field(0),
-      new Field(0),
-      new Field(0),
-      new Field(0),
-      new Field(0),
-      new Field(0),
       new Field(0)
+      // new Field(0),
+      // new Field(0),
+      // new Field(0),
+      // new Field(0),
+      // new Field(0),
+      // new Field(0),
+      // new Field(0),
+      // new Field(0)
     );
 
     this.pep.set(p);
   }
 
-  @method updateA1(a1: Field) {
+  @method updateA1(asd: Field) {
     const pep: TestStruct = this.pep.getAndAssertEquals();
-    pep.a1 = a1;
+    pep.a1 = asd;
+    // pep.seta1(asd)
+    // pep.seta1(asd)
     this.pep.set(pep);
   }
-  @method updateA2(a2: Field) {
-    const pep: TestStruct = this.pep.getAndAssertEquals();
-    pep.a2 = a2;
-    this.pep.set(pep);
-  }
-  @method updateA3(a3: Field) {
-    const pep: TestStruct = this.pep.getAndAssertEquals();
-    pep.a3 = a3;
-    this.pep.set(pep);
-  }
-  @method updateA4(a4: Field) {
-    const pep: TestStruct = this.pep.getAndAssertEquals();
-    pep.a4 = a4;
-    this.pep.set(pep);
-  }
+  // @method updateA2(a2: Field) {
+  //   const pep: TestStruct = this.pep.getAndAssertEquals();
+  //   pep.a2 = a2;
+  //   this.pep.set(pep);
+  // }
+  // @method updateA3(a3: Field) {
+  //   const pep: TestStruct = this.pep.getAndAssertEquals();
+  //   pep.a3 = a3;
+  //   this.pep.set(pep);
+  // }
+  // @method updateA4(a4: Field) {
+  //   const pep: TestStruct = this.pep.getAndAssertEquals();
+  //   pep.a4 = a4;
+  //   this.pep.set(pep);
+  // }
 
   // @method data(): [Field, Field, Field] {
   //   const pep: TestStruct = this.pep.getAndAssertEquals();
   //   return [pep.issuer, pep.expiration, pep.account]
   // }
 
-  @method f(): Field {
-    const pep: TestStruct = this.pep.getAndAssertEquals();
+  // @method f(): Field {
+  //   const pep: TestStruct = this.pep.getAndAssertEquals();
 
-    return pep._field0;
-  }
+  //   return pep._field0;
+  // }
 }
